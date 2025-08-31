5d94b0cea0efa0f24236b68c8e2c9726d705c528
Victor <kitoy.osu@mail.ru>
2025-08-31 04:26:14 +0200

    update footer content

--- client/src/Mainpage/footer/Footerblock.js
+++ client/src/Mainpage/footer/Footerblock.js
@@ -22,39 +22,41 @@ function Footerblock({ refs }) {
     <footer ref={footerRef}>
       <h2 className="footerLogo">Critiqo</h2>
 
-      <div className="navigate">
-        <ul>
-          <li onClick={() => scrollWithOffset(refs.aboutRef)}>О нас</li>
-          <li onClick={() => scrollWithOffset(refs.advantagesRef)}>Наши преимущества</li>
-          <li onClick={() => scrollWithOffset(refs.tryFreeRef)}>Попробовать бесплатно</li>
-          <li onClick={() => scrollWithOffset(refs.matchAnalysisRef)}>Разбор матчей</li>
-        </ul>
+      <div className='footer-top'>
+          <div className="navigate">
+          <ul>
+            <li onClick={() => scrollWithOffset(refs.aboutRef)}>О нас</li>
+            <li onClick={() => scrollWithOffset(refs.advantagesRef)}>Наши преимущества</li>
+            <li onClick={() => scrollWithOffset(refs.tryFreeRef)}>Попробовать бесплатно</li>
+            <li onClick={() => scrollWithOffset(refs.matchAnalysisRef)}>Разбор матчей</li>
+          </ul>
+        </div>
+
+        <div className="contacts">
+          <h3>Связаться с нами</h3>
+          <ul>
+            <li>
+              <a href="https://mail.ru/">CritiqoSupport@mail.ru</a>
+            </li>
+            <Link to="/support">
+              <li>Написать в техподдержку</li>
+            </Link>
+          </ul>
+        </div>
+
+        <div className="policy">
+          <ul>
+            <Link to="/">
+              <li>Политика конфиденциальности</li>
+            </Link>
+            <Link to="/">
+              <li>Условия использования</li>
+            </Link>
+          </ul>
+        </div>
       </div>
 
-      <div className="contacts">
-        <h3>Связаться с нами</h3>
-        <ul>
-          <li>
-            <a href="https://mail.ru/">CritiqoSupport@mail.ru</a>
-          </li>
-          <Link to="/support">
-            <li>Написать в техподдержку</li>
-          </Link>
-        </ul>
-      </div>
-
-      <div className="policy">
-        <ul>
-          <Link to="/">
-            <li>Политика конфиденциальности</li>
-          </Link>
-          <Link to="/">
-            <li>Условия использования</li>
-          </Link>
-        </ul>
-      </div>
-
-      <h2>© 2025 Critiqo</h2>
+      <h2 className='CritiqoAP'>© 2025 Critiqo</h2>
     </footer>
   );
 }
